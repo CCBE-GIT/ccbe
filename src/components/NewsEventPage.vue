e55acd42ee921f4cdf953193dafbdef9a495a6a8
Y. A. Perera <yunethperera987@gmail.com>
2026-03-17 12:30:44 +0530

    Update NewsEventPage
    adding galdoowa pinkam images

--- src/components/NewsEventPage.vue
+++ src/components/NewsEventPage.vue
@@ -87,6 +87,13 @@ export default {
           show: false,
         },
         {
+          image: "https://ik.imagekit.io/u3wbiya66/News&Events/NE10.jpeg",
+          title: "Galdoowa Buddhist Monastery 2026",
+          subtitle: "A Day of Meritorious Deeds & Unity",
+          content: "On this Madin Full Moon Poya Day, the staff and management of CCBE and CCIT came together to successfully organize our 2nd consecutive Charity Program at Kalyana Yogashramaya. \n\n Together, we were honored to conduct: \n ◾ Sathbudu Wandanawa Bodhi Poojawa \n ◾ Heel Daane (Breakfast) and Dahawal Daane (Lunch) offered to 50 venerable monks \n ◾ Meals and refreshments for over 750 Sil observers. \n\n This meaningful initiative was made possible through the dedication, generosity, and collective effort of our team. \n A heartfelt thank you to everyone who contributed their time, support, and kindness to make this meritorious event a success.",
+          show: false,
+        },
+        {
           image: "https://ik.imagekit.io/u3wbiya66/News&Events/NE1.jpeg",
           title: "CAMBRIANS DAY 2026",
           subtitle: "Celebrating Unity, Talent & the Spirit of Cambrians",
